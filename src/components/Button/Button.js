9e3d8dfcb85b1be99ce420b78a192cac98161ce4
Mateusz <mrpeciak@gmail.com>
2019-03-27 16:26:15 +0100

    update Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) => {
+    return(
+        <button className="button" onClick={props.onClickHandle} >{props.children}</button>
+    )
+}
+export default Button;
