f5de5fbb721ed0d903c977180b16110a1bb9db64
Mateusz <mrpeciak@gmail.com>
2019-03-27 19:53:48 +0100

    add onClickHandle property in Card and ResultCard components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
+import ResultCard from './components/ResultCard/ResultCard.js';
 
 class App extends Component {
   constructor() {
@@ -13,13 +14,28 @@ class App extends Component {
     };
     } 
 
+    changeCard = () => {
+      this.state.whichPage === 'result' ? this.setState({whichPage: 'main'}) : this.setState({whichPage: 'result'})
+    }
+    searchButton = () => {
+      this.changeCard();
+    }
+
 
   render() {
     return (
       <div>
-        <Card className={this.state.whichPage === 'main' ? 'box' : 'hidden'}/>
+        <Card 
+          className={this.state.whichPage === 'main' ? 'box' : 'hidden'} 
+          onClickHandle={this.searchButton}
 
-        
+          />
+        <ResultCard 
+          className={this.state.whichPage === 'main' ? 'hidden' : 'box'} 
+          onClickHandle={this.changeCard}
+            
+          />
+
       </div>
     );
   }
