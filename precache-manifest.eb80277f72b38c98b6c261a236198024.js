8582417c689ae86ac2e5894b9ea6ddcd7253ac22
Mateusz <mrpeciak@gmail.com>
2019-03-30 01:08:05 +0100

    Updates

---
+++ precache-manifest.eb80277f72b38c98b6c261a236198024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "98b5012ea55145c4c934",
+    "url": "/weather-app/static/js/main.6b791a6b.chunk.js"
+  },
+  {
+    "revision": "57a9a0213ece38b09370",
+    "url": "/weather-app/static/js/2.2c672f34.chunk.js"
+  },
+  {
+    "revision": "98b5012ea55145c4c934",
+    "url": "/weather-app/static/css/main.42237272.chunk.css"
+  },
+  {
+    "revision": "8fd3b561b429f9ed7a81fc1ce49c30ed",
+    "url": "/weather-app/index.html"
+  }
+];
